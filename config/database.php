057fc7f387e60d4ca136b34d28e2a764c0fbf693
Darnes1958 <abdel_ati_m@yahoo.com>
2025-06-02 21:16:38 +0200

    almakazen

--- config/database.php
+++ config/database.php
@@ -213,6 +213,21 @@ return [
             'prefix_indexes' => true,
             'trust_server_certificate' => true,
         ],
+        'Almakazen' => [
+            'driver' => 'sqlsrv',
+            'url' => env('DATABASE_URL_OTHER'),
+            'host' => env('DB_HOST_OTHER', 'localhost'),
+            'port' => env('DB_PORT_OTHER', '1433'),
+
+            'database' => env('DB_DATABASE_Almakazen', 'forge'),
+            'username' => env('DB_USERNAME_OTHER', 'forge'),
+            'password' => env('DB_PASSWORD_OTHER', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+
+            'prefix_indexes' => true,
+            'trust_server_certificate' => true,
+        ],
 
     ],
 
