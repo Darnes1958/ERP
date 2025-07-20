7860cea3a94c149beb3e641ce3c83859abcbdab0
unknown <bdel_ati_m@yahoo.com>
2025-07-20 14:00:01 +0200

    any

--- config/database.php
+++ config/database.php
@@ -197,6 +197,21 @@ return [
             'prefix_indexes' => true,
             'trust_server_certificate' => true,
         ],
+        'Shoshan' => [
+            'driver' => 'sqlsrv',
+            'url' => env('DATABASE_URL_OTHER'),
+            'host' => env('DB_HOST_OTHER', 'localhost'),
+            'port' => env('DB_PORT_OTHER', '1433'),
+
+            'database' => env('DB_DATABASE_Shoshan', 'forge'),
+            'username' => env('DB_USERNAME_OTHER', 'forge'),
+            'password' => env('DB_PASSWORD_OTHER', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+
+            'prefix_indexes' => true,
+            'trust_server_certificate' => true,
+        ],
 
         'Lamsa' => [
             'driver' => 'sqlsrv',
