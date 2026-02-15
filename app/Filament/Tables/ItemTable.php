3a6ccee007fcdd8ad040468a9ff20023d9d7f2e4
unknown <bdel_ati_m@yahoo.com>
2026-02-15 12:43:53 +0200

    any

--- app/Filament/Tables/ItemTable.php
+++ app/Filament/Tables/ItemTable.php
@@ -13,13 +13,29 @@ class ItemTable
     public static function configure(Table $table): Table
     {
         return $table
-            ->query(fn (): Builder => Item::query())
-            
+            ->query(fn (): Builder => Item::query()->join('place_sticks', 'place_stocks.item_id', '=', 'items.id')
+            ->selectRaw('items.id,items.barcode,name,place_stocks.stock1'))
+            ->modifyQueryUsing(function (Builder $query) use ($table): Builder {
+                $arguments = $table->getArguments();
+
+                if ($placeId = $arguments['place_id'] ?? null) {
+                    $query->where('place_id', $placeId);
+                }
+
+                if ($noZero = $arguments['noZero'] ?? null) {
+                    if ($noZero==1)
+                     $query->where('stock1', '>', 0);
+                }
+
+                return $query;
+            })
+
             ->columns([
                 TextColumn::make('id')->searchable()->sortable(),
                 TextColumn::make('barcode')->searchable()->sortable(),
                 TextColumn::make('name')->searchable()->sortable(),
             ])
+
             ->filters([
                 //
             ])
